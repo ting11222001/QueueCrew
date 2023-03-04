dac2bbac38cb094a7dff3516798fb83753a4d105
Thomas-Frew <thomasfrew13@gmail.com>
2023-03-04 17:14:59 +1030

    Buttons now link to pages

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,17 @@
 interface Props {
     name:string, 
     background:string
+    link:string
 }
 
-function Button({name, background}:Props) {
+function Button({name, background, link}:Props) {
   return (
-    <div className="h-[150px] w-[300px] m-[100px] rounded-[10px] flex place-items-center justify-center" style={{backgroundColor: `${background}`}}>
-        <p className="flex text-center text-5xl">{name}</p>
-    </div>
+    <a href={link}>
+      <div className="transition ease-in-out hover:scale-105 h-[150px] w-[300px] m-[100px] rounded-[10px] drop-shadow-lg flex place-items-center justify-center" style={{backgroundColor: `${background}`}}>
+          <p className="flex text-center text-5xl">{name}</p>
+      </div>
+    </a>
+
   )
 }
 
